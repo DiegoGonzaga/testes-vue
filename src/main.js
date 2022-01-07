6e7ac61db17041138e6249fe67693cf2eeea771b
Diego de Melo Gonzaga <diego.gonzaga.prestador@alleasy.com.br>
2022-01-07 18:01:44 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import { createRouter } from "vue-router";
+// import { BootstrapVue } from "bootstrap-vue";
 
-Vue.config.productionTip = false
+//import Form from "./components/Form.vue";
+const Bar = { template: "<div>bar</div>" };
+const routes = [
+  //{ path: "/login", component: Form },
+  { path: "/", component: App },
+  { path: "/bar", component: Bar },
+];
 
+const router = new createRouter({
+  routes, // short for `routes: routes`
+});
+
+// Vue.config.productionTip = false;
+// Vue.use(BootstrapVue);
+// Vue;
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  //render: (h) => h(App),
+}).$mount("#app");
