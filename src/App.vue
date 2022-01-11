77ee2bccb670c7110ec7e496ebfd001a42f81d86
Diego de Melo Gonzaga <diego.gonzaga.prestador@alleasy.com.br>
2022-01-11 13:50:52 -0300

    Created component NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
-    <div id="app">
-      <router-link to="/">Home</router-link> -
-      <router-link to="/login">Login</router-link> -
-      <router-link to="/profile">Profile</router-link>
-    </div>
+    <NavBar />
+    <!-- <div class="menuBar">
+      <img class="logo" src="logo.jpg" />
+      <div id="app" class="nav">
+        <ul>
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link to="/login">Login</router-link> -</li>
+          <li><router-link to="/profile">Profile</router-link></li>
+        </ul>
+      </div>
+    </div> -->
     <div id="container">
       <router-view></router-view>
     </div>
@@ -12,9 +18,12 @@
 </template>
 
 <script>
+import NavBar from "@/components/NavBar";
+
 export default {
   el: "#app",
   name: "app",
+  components: { NavBar },
 };
 </script>
 
@@ -24,6 +33,7 @@ export default {
 }
 #container {
   align-content: center;
+  height: 100%;
 }
 html {
   height: 100%;
@@ -32,7 +42,26 @@ body {
   min-height: 100%;
 }
 
-a:visited {
+/* a:visited {
   color: blanchedalmond;
 }
+.menuBar {
+  width: 100%;
+  height: 50px;
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+.logo {
+  float: left;
+}
+.nav {
+  float: right;
+  margin-right: 10px;
+}
+.nav ul li {
+  list-style: none;
+  float: left;
+} */
 </style>
